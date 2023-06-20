1bb5e638a585cf6a36d7373f5fb9549a49349039
xiemenwaidao <menxielijiao@gmail.com>
2023-06-20 12:28:10 +0900

    💄 【Header】MUIで置換え

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,165 @@
 import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
-import Link from "next/link";
 import { SITE } from "~/config";
-import { ThemeButton } from "./ThemeButton";
 
-export const Header = () => {
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { useState } from "react";
+import Link from "@mui/material/Link";
+
+const pages = [{ title: "Post" }];
+
+function Header() {
     const { user } = useUser();
 
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
-        <header className="flex justify-between  p-4">
-            <div className="flex gap-4">
-                <Link href={`/`}>
-                    <h1 className="font-bold">{SITE.title}</h1>
-                </Link>
-                {user && <Link href={`/post/new`}>new post</Link>}
-            </div>
-            <div className="flex gap-4">
-                <ThemeButton />
-                {user ? (
-                    <UserButton afterSignOutUrl={`localhost:300`} />
-                ) : (
-                    <SignInButton />
-                )}
-            </div>
-        </header>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <AdbIcon
+                        sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+                    />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}
+                    >
+                        {SITE.title}
+                    </Typography>
+
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "flex", md: "none" },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "left",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "left",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: "block", md: "none" },
+                            }}
+                        >
+                            {pages.map((page, index) => (
+                                <MenuItem
+                                    key={index}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        <Link
+                                            href={`/post/new`}
+                                            underline="hover"
+                                        >
+                                            {page.title}
+                                        </Link>
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon
+                        sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+                    />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: "flex", md: "none" },
+                            flexGrow: 1,
+                            fontFamily: "monospace",
+                            fontWeight: 700,
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}
+                    >
+                        {SITE.title}
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", md: "flex" },
+                        }}
+                    >
+                        {pages.map((page, index) => (
+                            <Button
+                                key={index}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: "white", display: "block" }}
+                            >
+                                <Link
+                                    href={`/post/new`}
+                                    underline="hover"
+                                    color="white"
+                                >
+                                    {page.title}
+                                </Link>
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        {user ? (
+                            <UserButton afterSignOutUrl={`localhost:300`} />
+                        ) : (
+                            <SignInButton />
+                        )}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
-};
+}
+export default Header;
