934e89877a6ed830e6d71a142e7c7e6478df7ee8
xiemenwaidao <menxielijiao@gmail.com>
2023-06-27 13:39:45 +0900

    💄 Add Footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,22 @@
 import type { PropsWithChildren } from "react";
 import Header from "./Header";
+import { Container, Grid } from "@mui/material";
+import Footer from "./Footer";
 
 export const Layout = (props: PropsWithChildren) => {
     return (
-        <>
-            <Header />
-            <main className="">{props.children}</main>
-        </>
+        <Grid container sx={{ minHeight: "100vh" }} direction={`column`}>
+            <Grid item xs="auto">
+                <Header />
+            </Grid>
+            <Grid item xs>
+                <Container component={`main`} maxWidth="md">
+                    {props.children}
+                </Container>
+            </Grid>
+            <Grid item xs="auto">
+                <Footer />
+            </Grid>
+        </Grid>
     );
 };
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 export const SITE = {
     title: "羊肉串在哪里",
+    description: "羊肉串大好き！",
+    url: "https://www.yangrouchuan.com",
 } as const;
 
 export const PALETTE_M0DE_STORAGE_KEY = "palette_mode";
