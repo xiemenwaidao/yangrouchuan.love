f8fe5f7d4a14bc3e7578c0dc2fccbb96ae37326b
xiemenwaidao <menxielijiao@gmail.com>
2023-06-30 12:55:37 +0900

    💄 Clerkダークモード対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,25 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 import Head from "next/head";
 import { SITE } from "~/config";
 import { Toaster } from "react-hot-toast";
-import { jaJP } from "@clerk/localizations";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Clerk from "~/components/Clerk";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
     return (
-        <ClerkProvider {...pageProps} localization={jaJP}>
-            <CssVarsProvider defaultMode="system">
+        <CssVarsProvider defaultMode="system">
+            <Clerk pageProps={pageProps}>
                 <Head>
                     <title>{`${SITE.title}`}</title>
                 </Head>
                 <CssBaseline />
                 <Toaster position="bottom-center" />
                 <Component {...pageProps} />
-            </CssVarsProvider>
-        </ClerkProvider>
+            </Clerk>
+        </CssVarsProvider>
     );
 };
 
