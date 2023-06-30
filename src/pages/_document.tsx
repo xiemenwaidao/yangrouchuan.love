0fb577196bb28e63778dabd5ed92eb557b842d0b
xiemenwaidao <menxielijiao@gmail.com>
2023-06-30 18:35:18 +0900

    🐛 _document.tsxの記述ミスってた

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    type DocumentContext,
+} from "next/document";
 import { getInitColorSchemeScript } from "@mui/material/styles";
 
-export default function Document() {
-    return (
-        <Html lang="ja">
-            <Head>
-                <meta name="description" content="羊肉串爱你们" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <body>
-                {getInitColorSchemeScript()}
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html lang="ja">
+                <Head>
+                    <meta name="description" content="羊肉串爱你们" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    {getInitColorSchemeScript()}
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
